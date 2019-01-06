29000e1fc4b6c5f2e132c4677a66eee80955a1fc
Amund Tveit <atveit@gmail.com>
2019-01-06 21:39:59 +0100

    basic modal window created

--- js/app.js
+++ js/app.js
@@ -227,12 +227,42 @@ function clearStars() {
 }
 
 /*
- * @description Creates popup when celebrating victory
+ * @description Creates popup when celebrating victory (DEPRECATED)
  */
  function congratulationsPopup() {
  	window.alert('congratulations finalizing the game!');
 	// perhaps reset afterwards?
 }
+
+function congratulationsModal() {
+	console.log('congratulationsModal')
+	  // Get the modal
+	var modal = document.querySelector('.modal');
+	console.log(modal);
+
+		// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+	console.log(span);
+
+	// open the modal window
+	modal.style.display = "block";
+
+
+	
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+	    modal.style.display = "none";
+	  }
+	}
+
+}
+
 
 /*
  * @description Adds event listener for cards
@@ -338,10 +368,12 @@ function updateTimer() {
 function main() {
 	updateStars(); // initially 3 stars before any move
 	displayCards();
+	congratulationsModal();
+		/*
 	setupRestartButton();
 	setupEventListenerForCards(); 
 	setTimeout(updateTimer, 1000);
-
+*/
 }
 
 main();
